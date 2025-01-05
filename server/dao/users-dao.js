a0db2722ccf1e64f68365ff535afe7679b18c6e1
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2025-01-05 01:20:37 +0100

    Flyio persistant db changes. Test for users only.

--- server/dao/users-dao.js
+++ server/dao/users-dao.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./database/OpenPage");
+const db = new sqlite3.Database("./volume/OpenPage");
 
 function listAll() {
   return new Promise((resolve, reject) => {
